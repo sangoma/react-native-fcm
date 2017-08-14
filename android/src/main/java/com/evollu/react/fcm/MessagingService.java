a810b1dcaaed3fe6719f8ee2dec595aa6096ff42
Eduardo Sellanes <eduardo.sellanes@gmail.com>
2017-08-14 12:14:47 -0300

    AndroidInCall branch
    Start activity when context is null

--- android/src/main/java/com/evollu/react/fcm/MessagingService.java
+++ android/src/main/java/com/evollu/react/fcm/MessagingService.java
@@ -1,11 +1,15 @@
 package com.evollu.react.fcm;
 
 import java.util.Map;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.WindowManager;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -22,14 +26,18 @@ public class MessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d(TAG, "Remote message received");
+
+        Log.d(TAG, "Message received");
+        Log.d(TAG,"Notification message: "+ remoteMessage.getData().toString());
+
         Intent i = new Intent("com.evollu.react.fcm.ReceiveNotification");
         i.putExtra("data", remoteMessage);
+
         handleBadge(remoteMessage);
         buildLocalNotification(remoteMessage);
 
         final Intent message = i;
-        
+
         // We need to run this on the main thread, as the React code assumes that is true.
         // Namely, DevServerHelper constructs a Handler() without a Looper, which triggers:
         // "Can't create handler inside thread that has not called Looper.prepare()"
@@ -39,14 +47,22 @@ public class MessagingService extends FirebaseMessagingService {
                 // Construct and load our normal React JS code bundle
                 ReactInstanceManager mReactInstanceManager = ((ReactApplication) getApplication()).getReactNativeHost().getReactInstanceManager();
                 ReactContext context = mReactInstanceManager.getCurrentReactContext();
+
                 // If it's constructed, send a notification
                 if (context != null) {
+                    //APP IS IN LOADED
                     context.sendOrderedBroadcast(message, null);
+
                 } else {
                     // Otherwise wait for construction, then send the notification
+                    // LOAD REACT INSTANCE
+
                     mReactInstanceManager.addReactInstanceEventListener(new ReactInstanceManager.ReactInstanceEventListener() {
                         public void onReactContextInitialized(ReactContext context) {
+                            //App was killed by Android or User, needs to startActivity
+                            launchApplication(context,"INCALL");
                             context.sendOrderedBroadcast(message, null);
+
                         }
                     });
                     if (!mReactInstanceManager.hasStartedCreatingInitialContext()) {
@@ -56,7 +72,20 @@ public class MessagingService extends FirebaseMessagingService {
                 }
             }
         });
+
     }
+
+
+    private void launchApplication(Context context, String codeID) {
+        String packageName = context.getApplicationContext().getPackageName();
+        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+
+        launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        launchIntent.putExtra("codeID", codeID);
+        context.startActivity(launchIntent);
+        Log.i(TAG, "Start activity with extra: Launching: (" + packageName + ")  : " + codeID );
+    }
+
 
     public void handleBadge(RemoteMessage remoteMessage) {
         BadgeHelper badgeHelper = new BadgeHelper(this);
